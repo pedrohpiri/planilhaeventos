744c81745a0379e0174892052863f2f11856b857
pedrohpiri <pedropiri.h@gmail.com>
2025-09-21 12:35:45 -0300

    Fix: update EventTable.vue to remove non-hierarchy names from third week and related corrections

--- src/components/Hierarchy.vue
+++ src/components/Hierarchy.vue
@@ -20,7 +20,7 @@ const hierarchy = [
     count: 1,
     icon: '🌞',
     members: [
-      { name: 'Gabiss', html: '@MASTER | 💔Gabiss' }
+      { name: 'Polly', html: '@ORGs | 🎭 Polly' }
     ]
   },
   {
@@ -28,7 +28,7 @@ const hierarchy = [
     count: 1,
     icon: '🌞',
     members: [
-      { name: 'Zack', html: '@ADM | Zack #GCP🏆' }
+      { name: 'Zack', html: '@ADM | Zack #EvT🏆' }
     ]
   },
   {
@@ -36,7 +36,7 @@ const hierarchy = [
     count: 1,
     icon: '🌞',
     members: [
-      { name: 'PH', html: '@Resp.EvT | 🐲 PH 20/24 | 8493' }
+      { name: 'PH', html: '@Resp.EvT | 💔  PH 20/24 | 8493' }
     ]
   },
   {
@@ -44,17 +44,15 @@ const hierarchy = [
     count: 1,
     icon: '🎉',
     members: [
-      { name: 'Sev777en', html: '@Aux.EvT | 🍀 Sev777en | 2450' }
+      { name: 'MarLock', html: '@AuX. EvT |🐲 MarLock #EvT| 1120' }
     ]
   },
   {
     name: 'Líder EvT',
-    count: 3,
+    count: 1,
     icon: '🚀',
     members: [
-      { name: 'Geraldo', html: '@Lid.EvT | 🐲Geraldo | 14158' },
-      { name: 'MarLock', html: '@Lid.EvT |🐲 MarLock#EvT🚀 | 1120' },
-      { name: 'Kira', html: '@Lid. EVT | 🐲Kira #GCP🏆 | 117' }
+      { name: 'Kira', html: '@Lid. EVT | 🐲Kira #EvT 🏆 | 117' }
     ]
   },
   {
@@ -63,39 +61,27 @@ const hierarchy = [
     icon: '🚀',
     members: [
       { name: 'Neggo trufa', html: '@Sub. EVT |🐲Neggo trufa | 1302' },
-      { name: 'Chucky', html: '@Sub.EvT| 🐲 Chucky #85% | 7809' }
+      { name: 'Ducra', html: '@Sub.EvT | 🐲 Ducra | 9959' }
     ]
   },
   {
     name: 'Equipe EvT',
-    count: 11,
+    count: 4,
     icon: '✨',
     members: [
-      { name: 'Camilla', html: '@EvT |🐉Camilla | 30224' },
-      { name: 'Carter', html: '@EvT | 🐲 Carter | 1176' },
-      { name: 'Luiz', html: '@EvT | 🐲 Luiz | 15427' },
-      { name: 'Matheus JM TD', html: '@EvT | 🐲 Matheus JM TD | 12074' },
-      { name: 'Rick Santana', html: '@EVT | 🐲 Rick Santana | 103' },
-      { name: 'Ruivo', html: '@EVT | 🐲 Ruivo | 102' },
-      { name: 'Gusta PLz', html: '@EvT | 🐲Gusta PLz | 4969' },
       { name: 'THX ZIKA', html: '@EvT | 🐲THX ZIKA | 12719' },
       { name: 'henrique muniz', html: '@EVT |🐲 henrique muniz | 11888' },
-      { name: 'Japones', html: '@EvT | 🐲 Japones | 31895' },
-      { name: 'Luluzinho Azul', html: '@EVT |🐲Luluzinho Azul | 25482' }
+      { name: 'Luluzinho Azul', html: '@EVT | 🐲Luluzinho Azul | 25482' },
+      { name: 'SOUZA', html: '@EvT | 🐲 SOUZA | 11814' }
     ]
   },
   {
     name: 'Equipe EvT-T',
-    count: 7,
+    count: 2,
     icon: '✨',
     members: [
-      { name: 'Eduardo Milk', html: '@EvT-T | 🐲Eduardo Milk | 6399' },
-      { name: 'Gabriel Magnata', html: '@EVT-T | 🐲Gabriel Magnata | 1954' },
-      { name: 'Matheus', html: '@EVT-T | 🐲Matheus | 11517' },
-      { name: 'CLEBER', html: '@EvT-T | 🐲CLEBER | 12555' },
-      { name: 'Nick Piloto', html: '@EvT-T | 🐲Nick Piloto | 16338' },
-      { name: 'Astro', html: '@EvT-T | 🐲Astro #AntXit | 34971' },
-      { name: 'Bernardo', html: '@EVT-T | 🐲 Bernardo| 19652' }
+      { name: 'CLEBER', html: '@EvT | 🐲CLEBER | 12555' },
+      { name: 'Nick Piloto', html: '@EvT-T | 🐲Nick Piloto | 16338' }
     ]
   }
 ]
